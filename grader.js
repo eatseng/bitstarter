c628cbe5b8cb8d7c369178fd608c3d797fcc1831
eatseng <axlinkck@aol.com>
2013-07-08 20:40:34 +0000

    final version of grader.js

--- grader.js
+++ grader.js
@@ -24,14 +24,18 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
+var TEMP = "temp.dat";
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = 'http://shrouded-cove-4178.herokuapp.com/';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -39,6 +43,38 @@ var assertFileExists = function(infile) {
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
+/*var cheerioURLFile = function(urladdress){
+    rest.get(urladdress).on('complete', function(result){
+	if(result instanceof Error){
+	    console.error('Error: ' + result.message);
+	}
+	else{
+	    console.error("Opened %s", urladdress);
+	    return cheerio.load(result);
+	}
+    });
+};*/
+
+var checkURLFile = function(urladdress, checksfile){
+    rest.get(urladdress).on('complete', function(result){
+	if(result instanceof Error){
+	    console.error('Error: ' + result.message);
+	}
+	else{
+	    console.error("Opened %s", urladdress);
+	    $=cheerio.load(result);
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    console.log(JSON.stringify(out, null, 4));
+	}
+    });
+};
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -49,11 +85,23 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
+
+/*var checkURLFile = function(URLfile, checksfile) {
+    $ = cheerioURLFile(URLfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};*/
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -61,14 +109,17 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url <url_address>', 'Path to url')
+	.parse(process.argv);
+    if(program.url){ var checkJson = checkURLFile(program.url, program.checks);}
+    else{ var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log(outJson);}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
